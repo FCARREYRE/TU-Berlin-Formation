933437ca7dd8461403c7da84d9a5b80b8fdc6de4
marcoliviermetais <48351289+marcoliviermetais@users.noreply.github.com>
2020-06-19 14:41:07 +0200

    congestion event handler : Ecriture des fichiers

--- src/main/java/org/matsim/run/UnterdenlitenCongestion.java
+++ src/main/java/org/matsim/run/UnterdenlitenCongestion.java
@@ -43,26 +43,27 @@ public class UnterdenlitenCongestion {
         MatsimEventsReader eventsReader = new MatsimEventsReader(eventsManager);
         eventsReader.readFile(inputFile);
 
-//        BerlinEventHandler.fileWriter();
+        BerlinEventHandler.fileWriter();
 
 
     }
 
     private static class BerlinEventHandler implements LinkEnterEventHandler,
-            LinkLeaveEventHandler, PersonDepartureEventHandler{
+            LinkLeaveEventHandler{
 
         private Map<Id<Vehicle>,Double> earliestLinkExitTime = new HashMap<>();
         private Network network;
-        Scanner sc = new Scanner(System.in);
-//        private Map<Id<Vehicle>, Double>
+        private static Map<Id<Vehicle>, Double> timeLostByVehicle = new HashMap<>();
+        private static Map<Id<Link>, Double> timeLostByLink = new HashMap<>();
+
 
         public BerlinEventHandler( Network network ) {
             this.network = network ;
         }
 
         public void handleEvent(LinkEnterEvent event){
-            System.out.println("LinkEnterEvent détecté véhicule n°" + event.getVehicleId()
-                    + "sur le link n°"+ event.getLinkId());
+//            System.out.println("LinkEnterEvent détecté véhicule n°" + event.getVehicleId()
+//                    + "sur le link n°"+ event.getLinkId());
             Link link = network.getLinks().get( event.getLinkId() ) ;
             double linkTravelTime = link.getLength() / link.getFreespeed( event.getTime() );
             this.earliestLinkExitTime.put( event.getVehicleId(), event.getTime() + linkTravelTime );
@@ -71,24 +72,55 @@ public class UnterdenlitenCongestion {
         }
 
         public void handleEvent(LinkLeaveEvent event){
-            System.out.println("LinkLeaveEvent détecté véhicule n°"+ event.getVehicleId()
-                    + "sur le link n°"+ event.getLinkId());
+//            System.out.println("LinkLeaveEvent détecté véhicule n°"+ event.getVehicleId()
+//                    + "sur le link n°"+ event.getLinkId());
             double leaveTime = event.getTime();
             Id<Vehicle> vehicleId = event.getVehicleId();
+            Id<Link> linkId = event.getLinkId();
             try {
-                double excessTravelTime = event.getTime() - this.earliestLinkExitTime.get(event.getVehicleId());
-                System.out.println("excess travel time: " + excessTravelTime);
+                double excessTravelTime = leaveTime - this.earliestLinkExitTime.get(vehicleId);
+//                System.out.println("excess travel time: " + excessTravelTime);
+                if(timeLostByVehicle.putIfAbsent(vehicleId, excessTravelTime) != null){
+                    timeLostByVehicle.put(vehicleId, timeLostByVehicle.get(vehicleId)+excessTravelTime);
+                }
+                if(timeLostByLink.putIfAbsent(linkId, excessTravelTime) != null){
+                    timeLostByLink.put(linkId, timeLostByLink.get(linkId)+excessTravelTime);
+                }
             }catch (Exception e){
-                System.out.println("Pas de LinkEnterEvent associé");
-                System.out.println("Appuyez sur une touche pour continuer");
-                sc.nextLine();
+                System.out.println("Départ du véhicule n°" + vehicleId);
             }
 
         }
 
-        public void handleEvent(PersonDepartureEvent event){
+        public static void fileWriter(){
+            System.out.println(timeLostByLink);
+            System.out.println(timeLostByVehicle);
 
+            for(Map.Entry<Id<Link>, Double> element : timeLostByLink.entrySet()){
+                try {
+//                    BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(".").toAbsolutePath().normalize().toString()+"/output_original_run/travelTimeByPerson.txt", true));
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(
+                            Paths.get(".").toAbsolutePath().normalize().toString()+"/output_dm1/timeLostByLink.txt", true));
+                    writer.append(element.getKey() + ";" + element.getValue() + "\n");
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            for(Map.Entry<Id<Vehicle>, Double> element : timeLostByVehicle.entrySet()){
+                try {
+//                    BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(".").toAbsolutePath().normalize().toString()+"/output_original_run/travelTimeByPerson.txt", true));
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(
+                            Paths.get(".").toAbsolutePath().normalize().toString()+"/output_dm1/timeLostByVehicle.txt", true));
+                    writer.append(element.getKey() + ";" + element.getValue() + "\n");
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
 
 
 
